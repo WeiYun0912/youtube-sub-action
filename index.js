3bb9bb2992fee76b73328eec6d3161b077276cd0
ChunWeiYeah <yher25@gmail.com>
2023-02-25 16:36:59 +0800

    test

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const puppeteer = require("puppeteer");
 async function getYoutubeSubNumber() {
   const browser = await puppeteer.launch({
     headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
   const page = await browser.newPage();
